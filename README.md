0a29457105446465497db35de17782373237d095
Alexander Horn <alexhorn@users.noreply.github.com>
2022-03-14 22:09:18 +0100

    Add script and readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# mensa-widget
-Scriptable widget that displays the TH Ingolstadt meal plan
+# THI Mensa Widget
+
+![Screenshot](screenshot.jpg)
+
+An iOS widget that displays today's meal plan for the TH Ingolstadt dining hall.
+
+## Installation
+
+1. Install [Scriptable](https://apps.apple.com/us/app/scriptable/id1405459188?uo=4)
+2. Create new script and paste the contents of [widget.js](/widget.js)
+3. Add new widget and select the created script
---
+++ widget.js
@@ -0,0 +1,45 @@
+async function createWidget() {
+  const req = new Request('https://neuland.app/api/mensa')
+  const [{ timestamp, meals }] = await req.loadJSON()
+  
+  const widget = new ListWidget()
+  widget.url = `https://neuland.app/food`
+  
+  const icon = widget.addText(`Mensa`)
+  icon.font = Font.boldSystemFont(16)
+  
+  widget.addSpacer(4)
+  
+  const date = widget.addText(new Date(timestamp).toLocaleDateString('de', { weekday: 'long', day: 'numeric', month: 'numeric' }))
+  date.font = Font.mediumSystemFont(14)
+
+  widget.addSpacer(8)
+  
+  for (const meal of meals) {
+    const stack = widget.addStack()
+    
+    const typeText = stack.addText(meal.name)
+    typeText.font = Font.systemFont(14)
+    
+    stack.addSpacer()
+    
+    const priceText = stack.addText(meal.prices.student.toLocaleString('de', { style: 'currency', currency: 'EUR' }))
+    priceText.font = Font.systemFont(14)
+    priceText.textColor = Color.gray()
+    
+    widget.addSpacer(8)
+  }
+  
+  widget.addSpacer()
+  
+  return widget
+}
+
+const widget = await createWidget()
+
+if (!config.runsInWidget) {
+  widget.presentMedium()
+}
+
+Script.setWidget(widget)
+Script.complete()
